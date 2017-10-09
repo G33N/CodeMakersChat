015cef348813e0b277423b23de46e343b49bceb2
G33N <mceliz@enlaceit.com.ar>
2017-10-09 01:51:58 -0300

    Added scroll and the social network buttons was fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { AngularFire, AuthProviders, AuthMethods, FirebaseListObservable } from 'angularfire2';
+
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,7 @@ import { AngularFire, AuthProviders, AuthMethods, FirebaseListObservable } from 
 export class AppComponent {
   items: FirebaseListObservable<any>;
   name: any;
+  loginWith: string;
   msgVal: string = '';
 
   constructor(public af: AngularFire) {
@@ -25,13 +27,15 @@ export class AppComponent {
     });
   }
 
-  login() {
+  loginFacebook() {
+    this.loginWith = 'facebook';
     this.af.auth.login({
       provider: AuthProviders.Facebook,
       method: AuthMethods.Popup
     })
   }
   loginGoogle() {
+    this.loginWith = 'google';
     this.af.auth.login({
       provider: AuthProviders.Google,
       method: AuthMethods.Popup
@@ -42,8 +46,27 @@ export class AppComponent {
   }
 
   chatSend(theirMessage: string) {
-    this.items.push({ message: theirMessage, name: this.name.google.displayName });
+    if (this.loginWith == 'google')
+      this.items.push({ message: theirMessage, name: this.name.google.displayName });
+    else
+      this.items.push({ message: theirMessage, name: this.name.facebook.displayName });
     this.msgVal = '';
   }
+
+  @ViewChild('scrollMe') public myScrollContainer: ElementRef;
+
+    ngOnInit() {
+        this.scrollToBottom();
+    }
+
+    ngAfterViewChecked() {
+        this.scrollToBottom();
+    }
+
+    scrollToBottom(): void {
+        try {
+            this.myScrollContainer.nativeElement.scrollTop = this.myScrollContainer.nativeElement.scrollHeight;
+        } catch(err) { }
+    }
 
 }
